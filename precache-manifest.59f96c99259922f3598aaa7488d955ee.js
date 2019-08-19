1ee3423228a2276d606fdd1a4b66d61315ede2f9
om <om@bigappcompany.in>
2019-08-19 17:39:59 +0530

    Updates

---
+++ precache-manifest.59f96c99259922f3598aaa7488d955ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f023e2c5a24622540e3bd2a7a5bdbd8",
+    "url": "/github-test-deploy/index.html"
+  },
+  {
+    "revision": "442908fdbdb5ba87d8de",
+    "url": "/github-test-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "44aa04203c7d6d1054e5",
+    "url": "/github-test-deploy/static/js/2.fdc07f50.chunk.js"
+  },
+  {
+    "revision": "442908fdbdb5ba87d8de",
+    "url": "/github-test-deploy/static/js/main.af5f4cf1.chunk.js"
+  },
+  {
+    "revision": "abbfd7739d95472d0882",
+    "url": "/github-test-deploy/static/js/runtime~main.4c9f878a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
